e6657c4e6786cb991b9424fd4950e6eef3d5de86
princenyamadi <princenyamadi.pn@gmail.com>
2020-12-20 00:37:56 +0000

    [feat]: Styling widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:finance_app_flutter/transaction.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 void main() {
   runApp(MyApp());
@@ -38,10 +39,12 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // backgroundColor: Colors.blueGrey,
       appBar: AppBar(
         title: Text('Finance'),
       ),
       body: Column(
+        // mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
           Container(
@@ -55,7 +58,41 @@ class MyHomePage extends StatelessWidget {
           Column(
             children: transaction.map((tx) {
               return Card(
-                child: Text(tx.title),
+                child: Row(children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                    child: Text(
+                      '\$${tx.amount}',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Colors.purple),
+                    ),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.purple,
+                        width: 2,
+                      ),
+                    ),
+                    padding: EdgeInsets.all(10),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        tx.title,
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        DateFormat().add_yMMMd().format(tx.date),
+                        style: TextStyle(color: Colors.grey),
+                      )
+                    ],
+                  )
+                ]),
               );
             }).toList(),
           ),
