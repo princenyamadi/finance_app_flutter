5fcbaf303fbe6ff7eb0a86b81fedfb5b5d40850f
princenyamadi <princenyamadi.pn@gmail.com>
2021-01-07 23:00:48 +0000

    Creating bars for chart

--- lib/widget/chart.dart
+++ lib/widget/chart.dart
@@ -3,42 +3,43 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 class Chart extends StatelessWidget {
-final List<Transaction> recentTransactions;
+  final List<Transaction> recentTransactions;
 
-Chart(this.recentTransactions);
+  Chart(this.recentTransactions);
 
+  List<Map<String, Object>> get groupedTransactionValues {
+    return List.generate(7, (index) {
+      final weekDay = DateTime.now().subtract(
+        Duration(days: index),
+      );
+      double totalSum;
 
-
-List<Map<String,Object>> get groupedTransactionValues{
-  return List.generate(7, (index){
-    final weekDay = DateTime.now().subtract(Duration(days: index),);
-    double totalSum;
-
-    for(var i = 0; i < recentTransactions.length; i++){
-      if(recentTransactions[i].date.day == weekDay.day &&
-         recentTransactions[i].date.month == weekDay.month &&
-         recentTransactions[i].date.year == weekDay.year
-        ){
+      for (var i = 0; i < recentTransactions.length; i++) {
+        if (recentTransactions[i].date.day == weekDay.day &&
+            recentTransactions[i].date.month == weekDay.month &&
+            recentTransactions[i].date.year == weekDay.year) {
           totalSum += recentTransactions[i].amount;
+        }
       }
-    }
-    print(DateFormat.E(weekDay));
-    print(totalSum.toString());
-    return {
-      'day':DateFormat.E(weekDay),
-      'amount':totalSum
-    };
-  });
-}
+      print(DateFormat.E(weekDay));
+      print(totalSum.toString());
+      return {
+        'day': DateFormat.E().format(weekDay).substring(0, 1),
+        'amount': totalSum
+      };
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Card(
       elevation: 6,
       margin: EdgeInsets.all(20),
-      child:Row(
-        children: ,
-      ) ,
+      child: Row(
+        children: groupedTransactionValues.map((data) {
+          return Text('${data['day']}: ${data['amount']}');
+        }).toList(),
+      ),
     );
   }
 }
