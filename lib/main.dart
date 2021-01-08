b11a8f48344a641f5698e74400598382a9cc65df
princenyamadi <princenyamadi.pn@gmail.com>
2021-01-08 17:21:07 +0000

    Using the LayoutBuilder widget

--- lib/main.dart
+++ lib/main.dart
@@ -123,7 +123,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 height: (MediaQuery.of(context).size.height -
                         appBar.preferredSize.height -
                         MediaQuery.of(context).padding.top) *
-                    0.4,
+                    0.3,
                 child: Chart(_userTransactions),
               ),
             ),
@@ -131,7 +131,7 @@ class _MyHomePageState extends State<MyHomePage> {
               height: (MediaQuery.of(context).size.height -
                       appBar.preferredSize.height -
                       MediaQuery.of(context).padding.top) *
-                  0.4,
+                  0.7,
               child: TransactionList(_userTransactions, _deleteTransaction),
             ),
           ],
