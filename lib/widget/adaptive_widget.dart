2e49cf29c8b802e87057d6c41882442508d43837
princenyamadi <princenyamadi.pn@gmail.com>
2021-01-12 23:52:33 +0000

    custom adaptive widget

--- lib/widget/adaptive_widget.dart
+++ lib/widget/adaptive_widget.dart
@@ -5,27 +5,26 @@ import 'package:flutter/material.dart';
 class AdaptiveButton extends StatelessWidget {
   final String text;
   final Function handler;
-  
-  AdaptiveButton({this.handler, this.text});
 
+  AdaptiveButton({this.handler, this.text});
 
   @override
   Widget build(BuildContext context) {
     return Platform.isIOS
-                        ? CupertinoButton(
-                            onPressed: handler,
-                            child: Text(
-                              text,
-                              style: TextStyle(fontWeight: FontWeight.bold),
-                            ),
-                          )
-                        : FlatButton(
-                            textColor: Theme.of(context).primaryColor,
-                            onPressed:handler,
-                            child: Text(
-                              text,
-                              style: TextStyle(fontWeight: FontWeight.bold),
-                            ),
-                          ),
+        ? CupertinoButton(
+            onPressed: handler,
+            child: Text(
+              text,
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+          )
+        : FlatButton(
+            textColor: Theme.of(context).primaryColor,
+            onPressed: handler,
+            child: Text(
+              text,
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+          );
   }
 }
